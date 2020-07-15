64f7d93d16b948bfe3ad2bbf167d0373e3544b0e
GuilhermeErthal <guilhermeerthal@hotmail.com>
2020-07-15 14:07:46 -0300

    Context

---
+++ src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Toast: React.FC = () => {
+  return (
+    <Container>
+      <FiAlertCircle size={20} />
+
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button onClick={() => removeToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
